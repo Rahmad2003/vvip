c0a152e09a6504f0bfd5e32e35ddef21573fe2ac
Rahmad2003 <138507369+Rahmad2003@users.noreply.github.com>
2023-07-27 02:22:16 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -384,7 +384,7 @@ echo -e " ${z}│$NC$r ⇲ $NC${z} TIME          $blue=$NC $TIMEZONE${NC}"
 echo -e " ${z}│$NC$r ⇲ $NC${z} IP VPS        $blue=$NC $MYIP${NC}"
 echo -e " ${z}│$NC$r ⇲ $NC${z} DOMAIN        $blue=$NC $domain${NC}"
 echo -e " ${z}╰══════════════════════════════════════════════════════════╯${NC}"
-echo -e "  ${KIRI} ${CYAN}•TERIMAKASIH SUDAH MENGGUNAKAN SCRIP INI•${NC} ${KANAN}"
+echo -e "     ${KIRI} ${CYAN}•TERIMAKASIH SUDAH MENGGUNAKAN SCRIP INI•${NC} ${KANAN}"
 echo -e "       ───────────────────────────────────────────────${NC}" | lolcat 
 echo -e "           ${CYAN}SSH/OPENVPN${NC}    $y=$NC $ssh1${NC}" "$a"
 echo -e "           ${CYAN}VMESS/WS/GRPC${NC}  $y=$NC $vma$NC" "$a"
