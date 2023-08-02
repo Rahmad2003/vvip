9ca669cab6a80c04189da5908c930179d8afb799
Rahmad2003 <138507369+Rahmad2003@users.noreply.github.com>
2023-08-02 09:40:48 +0800

    Update mbot.sh

--- menu/mbot.sh
+++ menu/mbot.sh
@@ -15,7 +15,7 @@ echo -e "\e[32mloading...\e[0m"
 clear
 IP=$(wget -qO- icanhazip.com)
 dateToday=$(date +"%Y-%m-%d")
-USRSC=$(curl -sS https://https://raw.githubusercontent.com/YOGZKNTL/regip/main/ip | grep $MYIP | awk '{print $2}')
+USRSC=$(curl -sS https://raw.githubusercontent.com/Rahmad2003/izin/main/ip/regip/main/ip | grep $MYIP | awk '{print $2}')
 
 setup_bot() {
     switch=$(grep -i "switch" /root/.bckupbot | awk '{print $2}')
