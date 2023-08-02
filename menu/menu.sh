f2c98ef5ad4124d8210449c0dd1d1311c015a939
Rahmad2003 <138507369+Rahmad2003@users.noreply.github.com>
2023-08-02 23:25:45 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -381,7 +381,7 @@ else
 fi
 clear
 echo -e " ${z}╭══════════════════════════════════════════════════════════╮${NC}"
-echo -e " ${z}│$NC\033[41m                  • INFO SYSTEM VPS •                     $NC${z}│$NC"
+echo -e " ${z}│$NC\033[41m                   • INFO SYSTEM VPS •                    $NC${z}│$NC"
 echo -e " ${z}╰══════════════════════════════════════════════════════════╯${NC}"
 echo -e " ${z}╭══════════════════════════════════════════════════════════╮${NC}"
 echo -e " ${z}│ $NC$r ➤  $NC${z} SYSTEM OS     $blue=$NC $MODEL${NC}"
@@ -397,9 +397,9 @@ echo -e " ${z}╭═════════════════════
 echo -e " ${z}│  \033[0m ${BOLD}${GREEN}XXX${BIYellow} SSH${GREEN}  XXX  ${BIYellow}VMESS  ${GREEN}XXX  ${BIYellow}VLESS  ${GREEN}XXX  ${BIYellow}TROJAN${GREEN} XXX $NC "
 echo -e " ${z}│  \033[0m ${Blue}     $ssh1         $vma           $vla           $tra              $NC"
 echo -e " ${z}╰══════════════════════════════════════════════════════════╯${NC}"
-echo -e " ${z}╭────────────────╮╭──────────────────╮╭────────────────────╮${NC}"
-echo -e " ${z}│ ${NC}${z} SSH$NC : $resssh" "        ${z} NGINX$NC : $resngx" "        ${z} XRAY$NC : $resv2r      $NC${z}│$NC" 
-echo -e " ${z}╰────────────────╯╰──────────────────╯╰────────────────────╯${NC}"
+echo -e " ${z}╭════════════════╮╭══════════════════╮╭════════════════════╮${NC}"
+echo -e " ${z}│ ${NC}${z}   SSH$NC : $resssh" "      ${z} NGINX$NC : $resngx" "      ${z} XRAY$NC : $resv2r      $NC${z}│$NC" 
+echo -e " ${z}╰════════════════╯╰══════════════════╯╰════════════════════╯${NC}"C}"
 echo -e " ${z}│$NC\033[41m                       • LIST MENU •                      $NC${z}│$NC"
 echo -e " ${z}╭──────────────────────────────────────────────────────────╮${NC}"
 echo -e " ${z}│$NC [${r}01${NC}] SSH MENU$NC     ${z}│$NC [${r}08${NC}] DELL ALL EXP$NC${z} │$NC [${r}15${NC}] BCKP/RSTR   $NC${z}│$NC"
