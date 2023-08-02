36f25de6f1e1b7a08b4d19858e46e21668051b77
Rahmad2003 <138507369+Rahmad2003@users.noreply.github.com>
2023-08-02 09:46:20 +0800

    Update bot.sh

--- bot/bot.sh
+++ bot/bot.sh
@@ -14,7 +14,7 @@ BOT="https://www.yogztrx.site/"
 ipsaya=$(wget -qO- ipinfo.io/ip)
 data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/YOGZKNTL/regip/main/ip"
+data_ip="https://raw.githubusercontent.com/Rahmad2003/izin/main/ip"
 checking_sc() {
   useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
   if [[ $date_list < $useexp ]]; then
